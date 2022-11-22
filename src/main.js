40adc77dd94bcbcf2eb82b3a6d83a795848fd602
Felipe Silva <felipe.ferreira.silva1003@gmail.com>
2022-11-22 00:43:07 -0300

    feat: finish first step of cli

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import fs from 'fs';
 import ncp from 'ncp';
 import path from 'path';
 import { promisify } from 'util';
+import execa from 'execa';
+import Listr from 'listr';
+import { projectInstall } from 'pkg-install';
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
@@ -13,6 +16,16 @@ async function copyTemplateFiles(options) {
   });
 }
 
+async function initGit(options) {
+  const result = await execa('git', ['init'], {
+    cwd: options.targetDirectory,
+  });
+  if (result.failed) {
+    return Promise.reject(new Error('Failed to initialize git'));
+  }
+  return;
+}
+
 export async function createProject(options) {
   options = {
     ...options,
@@ -20,21 +33,27 @@ export async function createProject(options) {
   };
 
   const templateDir = path.resolve(__filename, '../../templates', options.template.toLowerCase());
-
   options.templateDirectory = templateDir;
-  console.log(templateDir);
 
   try {
     await access(templateDir, fs.constants.R_OK);
   } catch (err) {
-    console.log(err);
     console.error('%s Invalid template name', chalk.red.bold('ERROR'));
     process.exit(1);
   }
 
-  console.log('Copy project files');
-  await copyTemplateFiles(options);
+  const tasks = new Listr([
+    { title: 'Copy project files', task: () => copyTemplateFiles(options) },
+    { title: 'Initialize git', task: () => initGit(options), enabled: () => options.git },
+    {
+      title: 'Install dependencies',
+      task: () => projectInstall({ cwd: options.targetDirectory }),
+      skip: () =>
+        !options.runInstall ? 'Pass --install to automatically install dependencies' : undefined,
+    },
+  ]);
 
+  await tasks.run();
   console.log('%s Project ready', chalk.green.bold('DONE'));
   return true;
 }
